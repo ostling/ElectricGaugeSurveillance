67a11198ee99b5c2d76a696831070f5a9e8d917d
martin.ostling <martin.ostling@xdin.com>
2012-10-23 14:25:10 +0200

    Added fragment for main view, also added fragment that displays simple statiscs.

--- se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/ElectricGaugeSurveillanceMainActivity.java
+++ se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/ElectricGaugeSurveillanceMainActivity.java
@@ -5,16 +5,10 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class ElectricGaugeSurveillanceMainActivity extends Activity {
-
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electric_gauge_surveillance_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_electric_gauge_surveillance_main, menu);
-        return true;
     }
 }
--- se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/fragments/MainViewListFragment.java
+++ se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/fragments/MainViewListFragment.java
@@ -1,7 +1,32 @@
 package se.xdin.electricgaugesurveillance.fragments;
 
 import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainViewListFragment extends ListFragment {
-
+	
+	String[] listActivities = new String[] { "Simple statistics", "Last 24-hours", "Last week"
+			, "Last month", "Last year", "More history", "Settings"};
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_expandable_list_item_1, listActivities);
+		setListAdapter(adapter);
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+	}
 }
