118b8dd466e81cdc28f339628ce4c971d7dd7214
martin.ostling <martin.ostling@xdin.com>
2012-10-23 15:17:35 +0200

    Removed Constants class to instead use inbuilt support for Resources.

--- se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/DefinedConstants.java
+++
@@ -1,5 +0,0 @@
-package se.xdin.electricgaugesurveillance;
-
-public class DefinedConstants {
-	public static int defaultNumberOfTicksPerKWH = 1000;
-}
--- se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/models/Sensor.java
+++ se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/models/Sensor.java
@@ -1,9 +1,9 @@
 package se.xdin.electricgaugesurveillance.models;
 
-import se.xdin.electricgaugesurveillance.DefinedConstants;
+import se.xdin.electricgaugesurveillance.R;
 
 public class Sensor {
-	int ticksPerKWH = DefinedConstants.defaultNumberOfTicksPerKWH;
+	int ticksPerKWH = (int) R.string.default_number_of_ticks;
 
 	public void setTicksPerKWH(int ticksPerKWH) {
 		this.ticksPerKWH = ticksPerKWH;
