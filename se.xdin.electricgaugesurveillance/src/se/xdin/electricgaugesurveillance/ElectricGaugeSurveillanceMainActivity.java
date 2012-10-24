7205c25ee9e11f402b5bb0b1aa93befb876668c7
ac2277 <anders.cullbrand@xdin.com>
2012-10-24 10:00:20 +0200

    test igen

--- se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/ElectricGaugeSurveillanceMainActivity.java
+++ se.xdin.electricgaugesurveillance/src/se/xdin/electricgaugesurveillance/ElectricGaugeSurveillanceMainActivity.java
@@ -10,7 +10,8 @@ public class ElectricGaugeSurveillanceMainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electric_gauge_surveillance_main);
-        System.out.println("Test");
+        System.out.println("Test");        
+        System.out.println("Test3");
     }
 
     
